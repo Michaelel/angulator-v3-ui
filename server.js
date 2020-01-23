7f5678d43bcee1e96be9e9f0fa3e4a4efcdb0c51
michaelel <michaelel1411@gmail.com>
2020-01-23 11:42:09 +0200

    server config modified

--- server.js
+++ server.js
@@ -7,6 +7,10 @@ const app = express();
 // Serve only the static files form the dist directory
 app.use(express.static('./dist/angulator-v3-ui'));
 
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static('client/build'));
+}
+
 app.get('/*', function(req,res) {
 
   res.sendFile(path.join('./dist/angulator-v3-ui/index.html'));
