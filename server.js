c7c267731510f641c8440229c839f2cff49ea0d4
michaelel <michaelel1411@gmail.com>
2020-01-23 12:20:30 +0200

    config reworked, backed route changed

--- server.js
+++ server.js
@@ -1,56 +1,26 @@
-// //Install express server
-// const express = require('express');
-// const path = require('path');
-//
-// const app = express();
-//
-// // Serve only the static files form the dist directory
-// app.use(express.static('./dist/angulator-v3-ui'));
-//
-// app.get('/*', function(req,res) {
-//
-//   res.sendFile(path.join('./dist/angulator-v3-ui/index.html'));
-// });
-//
-// // Start the app by listening on the default Heroku port
-// app.listen(process.env.PORT || 8080);
 
-const router = require('koa-router')();
-const body = require('koa-body')({ text: false });
-const send = require('koa-send');
-const fs = require('fs');
+var express = require('express');
+var path = require('path');
+var bodyParser = require('body-parser');
+var routes = require("./routes");
 
-/**
- * Code about server routes ommited
- */
+var app = express();
 
-async function main(ctx, next) {
-  //All dynamic routes start with "/api"
-  if (/\/api\//.test(ctx.path)) {
-    try {
-      await next();
-    }
-    catch (error) {
-      if (error instanceof ApplicationError) {
-        logger.error(error, { data: error.data, stack: error.stack });
-        ctx.status = error.code;
-      } else {
-        ctx.status = 500;
-        logger.error(error.message, { stack: error.stack });
-      }
-    }
-    return;
-  } else {
-    //Not a dynamic route, serve static content
-    if ((ctx.path != "/") && (fs.existsSync('dist' + ctx.path))) {
-      await send(ctx, 'dist' + ctx.path);
-    } else {
-      await send(ctx, 'dist/index.html');
-    }
-  }
-}
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
 
-module.exports = app => {
-  app.use(main);
-  app.use(router.routes());
-};
+app.use(express.static(path.join(__dirname, 'dist')));
+
+app.use("/api",routes);
+
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, 'dist/index.html'));
+});
+
+// Initialize the app.
+var server = app.listen(process.env.PORT || 8080, function () {
+  var port = server.address().port;
+  console.log("App now running on port", port);
+});
+
+module.exports = app;
--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -2,7 +2,7 @@ import {LanguageToLocale} from '../app/shared/enums/language-to-locale.enum';
 
 export const environment = {
   production: true,
-  backendRoute: 'http://angulatorv3.loc/api/',
+  backendRoute: 'http://angulatorv3-backend.herokuapp.com/api/',
 
   locale: LanguageToLocale.ru,
 };
