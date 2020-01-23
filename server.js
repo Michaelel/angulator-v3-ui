1b06dc6ff9c139b8be04c2c2acc01f7f82f242c3
m.elyseev <m.elyseev@concert.ua>
2020-01-23 12:15:12 +0200

    koa config tried

--- server.js
+++ server.js
@@ -1,22 +1,56 @@
-//Install express server
-const express = require('express');
-const path = require('path');
+// //Install express server
+// const express = require('express');
+// const path = require('path');
+//
+// const app = express();
+//
+// // Serve only the static files form the dist directory
+// app.use(express.static('./dist/angulator-v3-ui'));
+//
+// app.get('/*', function(req,res) {
+//
+//   res.sendFile(path.join('./dist/angulator-v3-ui/index.html'));
+// });
+//
+// // Start the app by listening on the default Heroku port
+// app.listen(process.env.PORT || 8080);
 
-const app = express();
+const router = require('koa-router')();
+const body = require('koa-body')({ text: false });
+const send = require('koa-send');
+const fs = require('fs');
 
-// Serve only the static files form the dist directory
-app.use(express.static('./dist/angulator-v3-ui'));
+/**
+ * Code about server routes ommited
+ */
 
-if (process.env.NODE_ENV === 'production') {
-  app.use(express.static('client/build'));
+async function main(ctx, next) {
+  //All dynamic routes start with "/api"
+  if (/\/api\//.test(ctx.path)) {
+    try {
+      await next();
+    }
+    catch (error) {
+      if (error instanceof ApplicationError) {
+        logger.error(error, { data: error.data, stack: error.stack });
+        ctx.status = error.code;
+      } else {
+        ctx.status = 500;
+        logger.error(error.message, { stack: error.stack });
+      }
+    }
+    return;
+  } else {
+    //Not a dynamic route, serve static content
+    if ((ctx.path != "/") && (fs.existsSync('dist' + ctx.path))) {
+      await send(ctx, 'dist' + ctx.path);
+    } else {
+      await send(ctx, 'dist/index.html');
+    }
+  }
 }
 
-app.get('/*', function(req,res) {
-
-  res.sendFile(path.join('./dist/angulator-v3-ui/index.html'));
-});
-
-// Start the app by listening on the default Heroku port
-app.listen(process.env.PORT || 8080);
-
-console.log('asdsad');
+module.exports = app => {
+  app.use(main);
+  app.use(router.routes());
+};
