68e488efb83e78b5a179ba4769ca341817a93c1b
michaelel <michaelel1411@gmail.com>
2020-01-23 11:01:33 +0200

    settings for heroku added

---
+++ server.js
@@ -0,0 +1,18 @@
+//Install express server
+const express = require('express');
+const path = require('path');
+
+const app = express();
+
+// Serve only the static files form the dist directory
+app.use(express.static('./dist/angulator-v3-ui'));
+
+app.get('/*', function(req,res) {
+
+  res.sendFile(path.join('./dist/angulator-v3-ui/index.html'));
+});
+
+// Start the app by listening on the default Heroku port
+app.listen(process.env.PORT || 8080);
+
+console.log('asdsad');
