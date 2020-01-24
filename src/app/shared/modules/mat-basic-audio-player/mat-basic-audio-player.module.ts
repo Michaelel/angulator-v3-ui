681fa8ea191780873fb13564f35ccac0f09fc654
m.elyseev <m.elyseev@concert.ua>
2020-01-24 20:46:37 +0200

    font awesome removed

--- src/app/shared/modules/mat-basic-audio-player/mat-basic-audio-player.module.ts
+++ src/app/shared/modules/mat-basic-audio-player/mat-basic-audio-player.module.ts
@@ -2,11 +2,8 @@ import {NgModule} from '@angular/core';
 import {CommonModule} from '@angular/common';
 import {MatBasicAudioPlayerComponent} from './mat-basic-audio-player.component';
 import {MaterialModule} from '../material/material.module';
-import {FontAwesomeModule} from "@fortawesome/angular-fontawesome";
 import {MatSliderModule} from "@angular/material/slider";
 import {SecondsToMinutesPipe} from "./seconds-to-minutes.pipe";
-import {fas} from "@fortawesome/free-solid-svg-icons";
-import {library} from "@fortawesome/fontawesome-svg-core";
 
 
 @NgModule({
@@ -17,7 +14,6 @@ import {library} from "@fortawesome/fontawesome-svg-core";
   imports: [
     CommonModule,
     MaterialModule,
-    FontAwesomeModule,
     MatSliderModule,
   ],
   exports: [
@@ -25,7 +21,4 @@ import {library} from "@fortawesome/fontawesome-svg-core";
   ],
 })
 export class MatBasicAudioPlayerModule {
-  constructor() {
-    library.add(fas);
-  }
 }
